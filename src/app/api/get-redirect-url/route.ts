f21e030f354661252d9a3a46374de54d67f0b690
mlewandowski <131498564+mlewandowskim@users.noreply.github.com>
2023-11-03 17:39:56 +0100

    Fix build

--- src/app/api/get-redirect-url/route.ts
+++ src/app/api/get-redirect-url/route.ts
@@ -1,5 +1,4 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from 'next'
 import strava from "strava-v3";
 
 
@@ -7,9 +6,7 @@ import strava from "strava-v3";
 const STRAVA_ACCS = [
   {refreshToken: '8e05640c003911fa48b558109937303c347b521d'}
 ]
-export async function GET(
-  req: NextApiRequest,
-) {
+export async function GET() {
   const acc = STRAVA_ACCS[0]
   strava.config({
     redirect_uri: 'http://localhost:3000/strava-callback',
